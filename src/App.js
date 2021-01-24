7315602366026cdcdee3bd8a1071766d63c8b6e9
Estefania <nialupo87@gmail.com>
2021-01-24 17:20:21 -0500

    added the container and the files for each page, also created the routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { HashRouter as Router, Route } from "react-router-dom";
-
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./containers/About/About";
+import Contact from "./containers/Contact/Contact";
+import Home from "./containers/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Router basename="/">
-
-      <h1>Hello React</h1>
+        <Switch>
+          <Route path="/about" component={About}></Route>
+          <Route path="/contact" component={Contact}></Route>
+          <Route path="/" component={Home}/>
+        </Switch>
       </Router>
-      <h1>Hello React</h1>
     </div>
   );
 }
