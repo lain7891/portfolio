58d5f76d46a5419a62914b37ac00b3765b97df5a
Estefania <nialupo87@gmail.com>
2021-01-26 14:18:52 -0500

    got the navbar to work and routes to pass from one page to the next

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./containers/About/About";
 import Contact from "./containers/Contact/Contact";
 import Home from "./containers/Home/Home";
+import Navbar from "./component/Navbar/Navbar"
 
 function App() {
   return (
     <div className="App">
       <Router basename="/">
+        <Navbar/>
         <Switch>
           <Route path="/about" component={About}></Route>
           <Route path="/contact" component={Contact}></Route>
