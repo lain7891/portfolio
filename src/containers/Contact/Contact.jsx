3e3701c69c3973b529ddbad5b58adc93d32d434f
Estefania <nialupo87@gmail.com>
2021-01-27 23:48:07 -0500

    added style to contact form

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -5,7 +5,15 @@ form: {
     width: 800,
     paddingLeft: 100,
     fontFamily: "Caveat, cursive",
-   fontColor: "white"
+   color: "#ffffff",
+   paddingTop: 50
+},
+input: {
+
+    fontFamily: "Caveat, cursive",
+    color: "#ffffff",
+   
+    
 }
 }
 
@@ -15,38 +23,38 @@ const Contact = () => {
         <div className="columns" style={styles.form}>
             <div className="column">
             <div className="field">
-        <label className="label">Name</label>
+        <label className="label subtitle is-3" style={styles.input}>Name</label>
         <div className="control">
-          <input className="input" type="text" placeholder="Text input"/>
+          <input className="input" type="text" placeholder="Text input" style={styles.input}/>
         </div>
       </div>
       
       <div className="field">
-        <label className="label">Email</label>
-        <div className="control has-icons-left has-icons-right">
-          <input className="input is-danger" type="email" placeholder="Email input" value="hello@"/>
+        <label className="label subtitle is-3 has-icon-left" style={styles.input}>Email</label>
+    
+          <input className="input is-danger" type="email" placeholder="Email input" style={styles.input}/>
           <span className="icon is-small is-left">
             <i className="fas fa-envelope"></i>
           </span>
           <span className="icon is-small is-right">
             <i className="fas fa-exclamation-triangle"></i>
           </span>
-        </div>
-        <p className="help is-danger">This email is invalid</p>
+      
+       
       </div>
       
       
       <div className="field">
-        <label className="label">Message</label>
+        <label className="label subtitle is-3" style={styles.input}>Message</label>
         <div className="control">
-          <textarea className="textarea" placeholder="Textarea"></textarea>
+          <textarea className="textarea" style={styles.input} placeholder="Textarea"></textarea>
         </div>
       </div>
       
       
       <div className="field is-grouped">
         <div className="control">
-          <button className="button is-link">Submit</button>
+          <button className="button is-info subtitle is-4" style={styles.input}>Submit</button>
         </div>
       </div>
             </div>
